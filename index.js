977b89a44fb7be79011e03a46f563077eb45c71b
md al rasel <mdalrasel96@gmail.com>
2024-09-02 09:09:33 +0600

    delete  operations successfully

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -30,7 +30,7 @@ async function run() {
         // Send a ping to confirm a successful connection
 
         const usersCollection = client.db("usersDB").collection("users");
-        
+
         app.get('/users',async(req,res)=>{
             const cursor =usersCollection.find()
             const result = await cursor.toArray()
@@ -40,7 +40,13 @@ async function run() {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
             res.send(result)
-
+        })
+        app.delete('/users/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query ={_id:new ObjectId(id)}
+            const result = await usersCollection.deleteOne(query);
+            res.send(result)
+            
         })
 
 
